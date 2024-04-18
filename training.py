0f951a868e42245750a230f645dc80d855d046a7
Vardan Barsegyan <91263358+vardanbarsegyan@users.noreply.github.com>
2024-04-18 20:28:43 +0200

    Update training.py
    Training 1

--- training.py
+++ training.py
@@ -17,20 +17,41 @@ def train_save_model(cleaned_df, outcome_df):
     outcome_df (pd.DataFrame): The data with the outcome variable (e.g., from PreFer_train_outcome.csv or PreFer_fake_outcome.csv).
     """
     
-    ## This script contains a bare minimum working example
-    random.seed(1) # not useful here because logistic regression deterministic
     
-    # Combine cleaned_df and outcome_df
-    model_df = pd.merge(cleaned_df, outcome_df, on="nomem_encr")
+# Get the best parameters and best model from grid search
+#best_params = grid_search.best_params_
+#best_model = grid_search.best_estimator_
+best_model = HistGradientBoostingClassifier(learning_rate=0.01)
 
-    # Filter cases for whom the outcome is not available
-    model_df = model_df[~model_df['new_child'].isna()]  
-    
-    # Logistic regression model
-    model = LogisticRegression()
+# Fit the model
+best_model.fit(X_train, y_train)
 
-    # Fit the model
-    model.fit(model_df[['age']], model_df['new_child'])
 
-    # Save the model
-    joblib.dump(model, "model.joblib")
+# Next
+best_model.score(X_train, y_train)
+best_model.predict(X_train)
+
+
+# Evaluate the model 
+visual1 = ConfusionMatrixDisplay.from_estimator(best_model, 
+                              X_train, 
+                              y_train)
+
+best_model.score(X_train, y_train)
+
+# Print ROC curve, it tells you how well you can balance false and true positives
+RocCurveDisplay.from_predictions(
+    y_train,
+    best_model.predict_proba(X_train)[:, 1],
+    color="cornflowerblue",
+)
+plt.plot([0, 1], [0, 1], "k--", label="chance level (AUC = 0.5)")
+plt.xlabel("False Positive Rate")
+plt.ylabel("True Positive Rate")
+plt.legend()
+plt.show()
+
+
+# Save the model
+joblib.dump(best_model, "best_model.joblib")
+
