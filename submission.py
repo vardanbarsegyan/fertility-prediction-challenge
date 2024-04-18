5d9324c708ef8d27d86ff6d7c8e127041440dd77
Vardan Barsegyan <91263358+vardanbarsegyan@users.noreply.github.com>
2024-04-18 20:27:35 +0200

    Update submission.py

--- submission.py
+++ submission.py
@@ -21,6 +21,31 @@ from sklearn.linear_model import LogisticRegression
 import joblib
 
 
+
+np.random.seed(13579)  
+
+
+# Load libraris 
+import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
+import shap
+#from sklearn.model_selection import train_test_split
+from sklearn.ensemble import HistGradientBoostingRegressor
+from sklearn.ensemble import HistGradientBoostingClassifier
+from sklearn.inspection import permutation_importance
+from sklearn.preprocessing import StandardScaler # preprocess numerical vars 
+from sklearn.model_selection import GridSearchCV
+from sklearn.experimental import enable_hist_gradient_boosting  # noqa
+from sklearn.linear_model import LogisticRegression
+import json # distionnaries 
+from sklearn.model_selection import cross_validate
+from sklearn.metrics import ConfusionMatrixDisplay
+from sklearn.metrics import RocCurveDisplay
+from joblib import dump, load
+
+
+
 def clean_df(df, background_df=None):
     """
     Preprocess the input dataframe to feed the model.
@@ -34,21 +59,93 @@ def clean_df(df, background_df=None):
     pd.DataFrame: The cleaned dataframe with only the necessary columns and processed variables.
     """
 
-    ## This script contains a bare minimum working example
-    # Create new variable with age
-    df["age"] = 2024 - df["birthyear_bg"]
 
-    # Imputing missing values in age with the mean
-    df["age"] = df["age"].fillna(df["age"].mean())
+#%%
+# Load the data 
+PreFer_train_data = pd.read_csv('/Users/vardanbarsegyan/Library/Mobile Documents/com~apple~CloudDocs/Papers/12_fertility_challenge/data/training_data/PreFer_train_data.csv', 
+                      encoding="cp1252", 
+                      low_memory=False)
+ 
+PreFer_train_outcome = pd.read_csv('/Users/vardanbarsegyan/Library/Mobile Documents/com~apple~CloudDocs/Papers/12_fertility_challenge/data/training_data/PreFer_train_outcome.csv', 
+                      encoding="cp1252", 
+                      low_memory=False)
 
-    # Selecting variables for modelling
-    keepcols = [
-        "nomem_encr",  # ID variable required for predictions,
-        "age"          # newly created variable
-    ] 
 
-    # Keeping data with variables selected
-    df = df[keepcols]
+PreFer_train_background_data = pd.read_csv('/Users/vardanbarsegyan/Library/Mobile Documents/com~apple~CloudDocs/Papers/12_fertility_challenge/data/other_data/PreFer_train_background_data.csv', 
+                      encoding="cp1252", 
+                      low_memory=False)
+
+
+# Load the codebook
+PreFer_codebook = pd.read_csv('/Users/vardanbarsegyan/Library/Mobile Documents/com~apple~CloudDocs/Papers/12_fertility_challenge/data/codebooks/PreFer_codebook.csv', 
+                      encoding="cp1252", 
+                      low_memory=False)
+
+PreFer_codebook_summary = pd.read_csv('/Users/vardanbarsegyan/Library/Mobile Documents/com~apple~CloudDocs/Papers/12_fertility_challenge/data/codebooks/PreFer_codebook_summary.csv', 
+                      encoding="cp1252", 
+                      low_memory=False)
+#%%
+# Explore the types of variables
+PreFer_codebook.type_var.value_counts()
+PreFer_codebook.columns
+PreFer_codebook.dataset.value_counts()
+
+
+
+#%%
+# Define the numeric and categoric variables
+PreFer_codebook_main = PreFer_codebook[PreFer_codebook['dataset'] == 'PreFer_train_data.csv']
+
+# Filter the codebook to get only numeric and categorical variables
+PreFer_codebook_main_types = PreFer_codebook_main[PreFer_codebook_main['type_var'].isin(['numeric', 'categorical'])]
+
+# Select these variables from the main dataset
+PreFer_train_data_vartypes = PreFer_train_data[PreFer_codebook_main_types['var_name'].tolist()]
+
+# numeric vars
+numeric_vars_temp = PreFer_codebook_main_types[PreFer_codebook_main_types['type_var'] == 'numeric']
+numeric_vars = numeric_vars_temp['var_name'].tolist()
+numeric_vars = [numeric_vars for numeric_vars in numeric_vars if numeric_vars != 'nomem_encr']
+
+
+# categorical_vars
+categorical_vars_temp = PreFer_codebook_main_types[PreFer_codebook_main_types['type_var'] == 'categorical']
+categorical_vars = categorical_vars_temp['var_name'].tolist()
+len(categorical_vars_temp) 
+
+
+
+#%%
+# ENCODE 
+# One-hot encode the categorical variables
+data_encoded = pd.get_dummies(PreFer_train_data_vartypes, 
+                              columns = categorical_vars)
+
+# Standardize the numerical variables
+scaler = StandardScaler()
+data_encoded[numeric_vars] = scaler.fit_transform(data_encoded[numeric_vars])
+
+#%%
+# Merge the X and y vars 
+merged_df = pd.merge(data_encoded, PreFer_train_outcome, on='nomem_encr', how='inner')
+
+
+#%%
+merged_df.new_child.value_counts()
+
+# Replace NaN values  with 0
+merged_df['new_child'] = merged_df['new_child'].fillna(0)
+
+
+
+# Define the features (X) and the target (y)
+X = merged_df.drop(columns='new_child')
+y = merged_df['new_child']
+
+# All data to train 
+X_train = X.copy() 
+y_train = y.copy()
+
 
     return df
 
